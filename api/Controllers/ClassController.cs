6499e252a1c16b8f22c7a75c2287838ad503bcf2
Quilixy <yldryd159@gmail.com>
2025-06-23 17:12:33 +0300

    6th: Class DTO&Controller

---
+++ api/Controllers/ClassController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using api.Data;
+using api.Dtos;
+using api.Models;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClassController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ClassController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("add")]
+        public IActionResult CreateClass([FromBody] CreateClassDto dto)
+        {
+            var classEntity = new Class
+            {
+                Id = dto.Id,
+                Description = dto.Description
+            };
+
+            _context.Classes.Add(classEntity);
+            _context.SaveChanges();
+            return Ok("Sınıf eklendi.");
+        }
+    }
+}
---
+++ api/DTOs/CreateClassDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Dtos
+{
+    public class CreateClassDto
+    {
+        public int    Id          { get; set; }          
+        public string Description { get; set; } 
+    }
+}
+
